f7b5b8096aa527ffcb92c9a086eb490eb6f673ac
sarahyaw <syaw@emich.edu>
2021-03-11 21:38:17 -0500

    Merged PRNGs into one

--- src/main/java/driver.java
+++ src/main/java/driver.java
@@ -12,13 +12,39 @@ public class driver
         int limit = in.nextInt();
         if (limit != -1){ max = limit;}
         else{ max = Integer.MAX_VALUE;}
-
         System.out.println("Seed entered was: "+seed);
-        System.out.println("Middle Squares string is: "+MiddleSquares(seed));
-        System.out.println("Lehmer string is: "+Lehmer(seed));
-        System.out.println("Linear Congruential string is: "+LinearCongruential(seed));
-        System.out.println("Inversive Congruential Generator string is: "+InversiveCongruentialGenerator(seed));
-        //System.out.println("xxxx string is: "+xxxx(seed));
+
+        //use middle squares to seed lehmer
+        String temp =MiddleSquares(seed);
+        //System.out.println("Middle Squares string is: "+temp);
+        int t = temp.length()/3;
+        int s = Integer.parseInt(temp.substring(0,5));
+        int m = Integer.parseInt(temp.substring(t,t+5));
+        int a = Integer.parseInt(temp.substring(2*t,2*t+5));
+        temp = Lehmer(s, m, a);
+
+        //use lehmer to seed inversive congruential
+        //System.out.println("Lehmer string is: "+temp);
+        t = temp.length()/3;
+        s = Integer.parseInt(temp.substring(0,5));
+        m = Integer.parseInt(temp.substring(t,t+5));
+        a = Integer.parseInt(temp.substring(2*t,2*t+5));
+        temp = InversiveCongruentialGenerator(s, m, a);
+
+        //use inversive congruential to seed linear
+        //System.out.println("Inversive Congruential Generator string is: "+temp);
+        t = temp.length()/3;
+        s = Integer.parseInt(temp.substring(0,5));
+        m = Integer.parseInt(temp.substring(t,t+5));
+        a = Integer.parseInt(temp.substring(2*t,2*t+5));
+        if(a<=Math.sqrt(m))
+            temp = LinearCongruential(s, m, a);
+        else
+            temp = LinearCongruential(s, m, a%m);
+
+        //LC gives the final output string
+        //System.out.println("Linear Congruential string is: "+temp);
+        System.out.println("Random string is: \n"+temp);
     }
 
     public static String MiddleSquares(int seed)
@@ -38,32 +64,24 @@ public class driver
         }
         return out;
     }
-    public static String Lehmer(int seed)
+    public static String Lehmer(int seed, int m, int a)
     {   //another very early PRNG algorithm
         String out="", temp="";
-        int current = seed, loops = 0, m=0, a=0;
-        System.out.print("Please enter a large prime integer: "); //debugging just uses 13
-        m = in.nextInt();
-        System.out.print("Please enter an integer: ");
-        a = in.nextInt();
+        int current = seed, loops = 0;
 
         while ((current!=seed || loops==0) && loops < max)
         {
-            temp=current+"";
+            temp=Math.abs(current)+"";
             out=out+temp;
             current=(a*current)%m;
             loops++;
         }
         return out;
     }
-    public static String LinearCongruential(int seed)
+    public static String LinearCongruential(int seed, int mod, int a)
     {   //Schrage's method of linear congruential generator. This is a generalization of Lehmer
         String out = "", temp = "";
-        int current = seed, loops = 0, a=0, mod=0,r=0, q=0;
-        System.out.print("Please enter a non-prime integer: ");
-        mod = in.nextInt();
-        System.out.print("Please enter an integer less than or equal to "+(int)Math.sqrt(mod)+" :");
-        a = in.nextInt();
+        int current = seed, loops = 0,r=0, q=0;
         r=mod%a;
         q=(mod-r)/a;
         while ((current!=seed || loops==0) && loops < max)
@@ -75,14 +93,10 @@ public class driver
         }
         return out;
     }
-    public static String InversiveCongruentialGenerator(int seed)
+    public static String InversiveCongruentialGenerator(int seed, int mod, int base)
     {
         String out="", temp="";
-        int current = seed, loops = 0, mod=0, i=1, j=2, base=0;
-        System.out.print("Please enter an integer to modulus: ");
-        mod = in.nextInt();
-        System.out.print("Please enter an integer for a base case: ");
-        base = in.nextInt();
+        int current = seed, loops = 0, i=1, j=2;
         while (j<=mod && loops<max)
         {
             if(current==0)
@@ -91,20 +105,6 @@ public class driver
                 current = (int) (Math.pow(current, i)+ Math.pow(current,j)) % mod;
             temp=current+"";
             out=out+temp;
-            loops++;
-        }
-        return out;
-    }
-    public static String xxxx(int seed)
-    {
-        String out="", temp="";
-        int current = seed, loops = 0;
-
-        while ((current!=seed || loops==0) && loops < max)
-        {
-            temp=current+"";
-            out=out+temp;
-            //next step
             loops++;
         }
         return out;
